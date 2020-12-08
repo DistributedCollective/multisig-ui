d85de53d34d7915dda12f107f6cb772377c674d7
Rytis Grincevicius <rytis.grincevicius@gmail.com>
2020-12-08 14:24:52 +0200

    changed multisign wallets

--- src/app/containers/BlockChainProvider/contracts.ts
+++ src/app/containers/BlockChainProvider/contracts.ts
@@ -4,13 +4,13 @@ import MultiSigWallet from './abi/MultiSigWallet.json';
 export const contracts: IContractNetworks = {
   testnet: {
     multiSigWallet: {
-      address: '0x189ecD23E9e34CFC07bFC3b7f5711A23F43F8a57',
+      address: '0x09deA2051575994E2B26772C0dC3Fa061AB760B3',
       abi: MultiSigWallet as any,
     },
   },
   mainnet: {
     multiSigWallet: {
-      address: '0x189ecD23E9e34CFC07bFC3b7f5711A23F43F8a57',
+      address: '0x08118a219a4e34E06176cD0861fcDDB865771111',
       abi: MultiSigWallet as any,
     },
   },
