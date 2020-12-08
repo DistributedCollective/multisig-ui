fa6003466a3154b3faad94b7e836a8e2d43ec59a
creed-victor <creed-victor@users.noreply.github.com>
2020-12-08 06:11:23 +0000

    Deploying to gh-pages from  @ f19b3f552c405dc2b6925ec890d38c6cb0ce32ee 🚀

---
+++ static/js/11.964e7621.chunk.js
@@ -0,0 +1,1 @@
+(this["webpackJsonpgovernance-dapp"]=this["webpackJsonpgovernance-dapp"]||[]).push([[11],{1290:function(n,e,t){"use strict";t.r(e),t.d(e,"Footer",(function(){return c}));var r=t(0),a=t.n(r);function c(){return a.a.createElement(a.a.Fragment,null)}}}]);
