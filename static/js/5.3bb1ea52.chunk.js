b67b1fc8b766b7d412fe6de62d285cbe467643e6
creed-victor <creed-victor@users.noreply.github.com>
2021-12-14 13:09:06 +0000

    Deploying to gh-pages from  @ 47f94531ff6a1cc2771e035e786bc44dc0bd0ef7 🚀

---
+++ static/js/5.3bb1ea52.chunk.js
@@ -0,0 +1,1 @@
+(this["webpackJsonpmultisig-ui"]=this["webpackJsonpmultisig-ui"]||[]).push([[5],{1726:function(t,n,i){"use strict";i.r(n),i.d(n,"Footer",(function(){return r}));var u=i(0),e=i.n(u);function r(){return e.a.createElement(e.a.Fragment,null)}}}]);
