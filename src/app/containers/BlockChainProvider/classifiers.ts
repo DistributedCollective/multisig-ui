86e529a7888c607f9068d5057107dfa73d4cedc8
Victor Creed <69458664+creed-victor@users.noreply.github.com>
2022-05-12 09:52:42 +0300

    Update classifiers.ts

--- src/app/containers/BlockChainProvider/classifiers.ts
+++ src/app/containers/BlockChainProvider/classifiers.ts
@@ -31,7 +31,7 @@ export const wssNodes = {
   1: 'wss://mainnet.infura.io/ws/v3/237d5a5403134af7b7211fd6996c15d3',
   3: 'wss://ropsten.infura.io/ws/v3/237d5a5403134af7b7211fd6996c15d3',
   30: 'wss://mainnet.sovryn.app/websocket',
-  31: 'wss://testnet.sovryn.app/ws',
+  31: 'wss://testnet.sovryn.app/websocket',
   56: 'wss://bsc-ws-node.nariox.org:443',
 };
 
